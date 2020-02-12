f6541d35043d4c132ce1412abe9ca962d514a6fb
Oscar <mundonatural@gmail.com>
2020-02-12 08:54:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,18 @@ function nowServing(deli) {
 
   return `Currently serving ${deli.shift()}.`;
 }
+
+function currentLine(deli) {
+  if (deli.length == 0) {
+    return "The line is currently empty.";
+  }
+
+  var devuelve = "The line is currently: ";
+  for (var i = 1; i < deli.length; i++) {
+    devuelve += `${i}. ${deli[i]}, `);
+  }
+  // incluye el ultimo, sin la coma final
+  devuelve += `${i}. ${deli[i]}`);
+
+  return devuelve;
+}
